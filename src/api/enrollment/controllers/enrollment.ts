6bc9d325516681595ef4b02973e4da51af89ff2a
farial-robama <farialrobama15@gmail.com>
2026-08-30 18:39:47 +0600

    fix(enrollment): set student relation from authenticated user server-side

--- src/api/enrollment/controllers/enrollment.ts
+++ src/api/enrollment/controllers/enrollment.ts
@@ -1,4 +1,19 @@
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::enrollment.enrollment');
+export default factories.createCoreController('api::enrollment.enrollment', ({ strapi }) => ({
+  async create(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized('You must be logged in to enroll.');
+    }
+
+    ctx.request.body.data = {
+      ...ctx.request.body.data,
+      student: user.id,
+    };
+
+    const response = await super.create(ctx);
+    return response;
+  },
+}));
